849c4a0114a6aaa03810f1ac0f8340fb7b552238
xujia <rocketxj@gmail.com>
2012-12-04 13:20:13 +0800

    ppt

---
+++ ppt/sample-2012-12-01.js
@@ -0,0 +1,12 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: xujia
+ * Date: 12-12-1
+ * Time: 下午5:45
+ * To change this template use File | Settings | File Templates.
+ */
+$(function() {
+    // Deck initialization
+    $.deck('.slide');
+});
+
