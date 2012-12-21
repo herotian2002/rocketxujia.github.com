b39cc9efbd6e7cba349a8e4ddaf76aa76793fb58
xujia <rocketxj@gmail.com>
2012-12-21 23:57:30 +0800

    github首页作品展示完成

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,33 @@
+// unGalleryContainer
+//!function(){
+//  var $box = $("#unGalleryContainer").show();
+//var all = ['<h2 class="title">作品墙</h2>'],
+//  workPics = ['css3-animation.jpg','js.jpg','3d-mm.jpg','gigi-gallery.jpg'],
+//  workTitles = ['教你玩转css3动画','js基础原理分享','图片旋转木马3D效果','美女Gigi图片墙'],
+//  hrefs =['http://rocketxujia.github.com/ppt/css3-animation.html','http://rocketxujia.github.com/ppt/js-2012-12-01.html',
+//    'http://rocketxujia.github.com/ppt/css3-animation.html#slide-5','http://rocketxujia.github.com/ppt/css3-animation.html#slide-3'],
+//  len = workPics.length;
+//  for (var i = 0; i < len; i++) {
+//    var top = parseInt(240 * Math.random() + 100), left=parseInt(600 * Math.random()), transform = 'transform' + top%10;
+//    all.push( '<a href="'+hrefs[i]+'" class="pic ' + transform + '" title="' + workTitles[i] + '" style="top:'+ top + 'px; left:' + left + 'px;"><h3>' + workTitles[i] + '</h3><img style="min-height:100px;" src="assets/img/' + workPics[i] + '" /></a>');
+//  }
+//  $box.html( all.join('') );
+//  $(document.body).addClass('body-wood');
+//}();
+
+//galleryContainer
 !function(){
-  var $box = $("#unGalleryContainer");
-  var all = ['<h5 class="title">作品墙</h5>'],
-    workPics = ['css3-animation.jpg'],
-    workTitles = ['玩转css3动画'],
+  var $box = $("#galleryContainer").show();
+  var all = ['<h2 class="title">作品墙</h2>'],
+    workPics = ['css3-animation.jpg','js.jpg','3d-mm.jpg','gigi-gallery.jpg'],
+    workTitles = ['教你玩转css3动画','js基础原理分享','图片旋转木马3D效果','美女Gigi图片墙'],
+    hrefs =['http://rocketxujia.github.com/ppt/css3-animation.html','http://rocketxujia.github.com/ppt/js-2012-12-01.html',
+    'http://rocketxujia.github.com/ppt/css3-animation.html#slide-5','http://rocketxujia.github.com/ppt/css3-animation.html#slide-3'],
     len = workPics.length;
   for (var i = 0; i < len; i++) {
-    var top = parseInt(500 * Math.random()), left=parseInt(960 * Math.random()), transform = 'transform' + top%10;
-    all.push( '<a href="#" class="pic ' + transform + '" title="' + workTitles[i] + '" style="top:'+ top + 'px; left:' + left + 'px;"><h3>' + workTitles[i] + '</h3><img style="min-height:100px;" src="assets/img/' + workPics[i] + '" /></a>');
+    var top = parseInt(240 * Math.random() + 100), left=parseInt(600 * Math.random());
+    all.push( '<a href="'+hrefs[i]+'" class="item" title="' + workTitles[i] + '" style=""><h3>' + workTitles[i] + '</h3><img style="min-height:100px;" src="assets/img/' + workPics[i] + '" /></a>');
   }
   $box.html( all.join('') );
+  $(document.body).addClass('deck-container');
 }();
